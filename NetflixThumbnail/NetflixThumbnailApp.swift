eb218e30683a6da088c53f04e457ef375109b768
yuseong <dog2655@naver.com>
2024-09-13 12:32:54 +0900

    Initial Commit

---
+++ NetflixThumbnail/NetflixThumbnailApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixThumbnailApp.swift
+//  NetflixThumbnail
+//
+//  Created by yuseong on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixThumbnailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
